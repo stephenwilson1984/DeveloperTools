3a7cad2295199c4727319c31ba3dd42c23da6bfa
Stephen Wilson <stephenwilson1984@outlook.com>
2024-10-01 20:07:35 +0100

    Add project files.

---
+++ DeveloperTools.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace DeveloperTools.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
